7a782bde1ee66889e78cc3f2b19a97bfc1fca331
umeguzojohnmichael <umeguzojohnmichael@gmail.com>
2018-07-12 16:06:34 +0100

    changes added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app); 
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('Server is up on 3000');
 });
 
